bd44d364a3d08403e6b8f6f856b87b8cfac691d8
wahidalqorni <idunalqorni@gmail.com>
2022-09-02 08:19:57 +0700

    ubah PenilaianController API

--- app/Http/Controllers/Api/PenilaianController.php
+++ app/Http/Controllers/Api/PenilaianController.php
@@ -14,42 +14,44 @@ use Illuminate\Support\Facades\Validator;
 
 class PenilaianController extends Controller
 {
-    public function index(){
+    public function index()
+    {
 
         $data = DB::table('users')
-                ->leftjoin('detail_grup_penilaians','detail_grup_penilaians.user_id', '=', 'users.id')
-                ->leftjoin('gruppenilaians','gruppenilaians.id', '=', 'detail_grup_penilaians.gruppenilaian_id')
-                ->leftjoin('penilaians', 'penilaians.detail_grup_penilaian_id', '=', 'detail_grup_penilaians.id')
-                ->select(
-                    'users.name',
-                    'users.jk', 
-                    'users.no_reg',
-                    
-                    'detail_grup_penilaians.id as detail_grup_penilaian_id',
-                    'penilaians.nilai_lari',
-                    'penilaians.jarak_lari',
+            ->leftjoin('detail_grup_penilaians', 'detail_grup_penilaians.user_id', '=', 'users.id')
+            ->leftjoin('gruppenilaians', 'gruppenilaians.id', '=', 'detail_grup_penilaians.gruppenilaian_id')
+            ->leftjoin('penilaians', 'penilaians.detail_grup_penilaian_id', '=', 'detail_grup_penilaians.id')
+            ->select(
+                'users.name',
+                'users.jk',
+                'users.no_reg',
 
-                    'penilaians.nilai_push_up',
-                    'penilaians.jumlah_push_up',
+                'detail_grup_penilaians.id as detail_grup_penilaian_id',
+                'penilaians.nilai_lari',
+                'penilaians.jarak_lari',
 
-                    'penilaians.nilai_sit_up',
-                    'penilaians.jumlah_sit_up',
+                'penilaians.nilai_push_up',
+                'penilaians.jumlah_push_up',
 
-                    'penilaians.nilai_shuttle_run',
-                    'penilaians.jumlah_shuttle_run'
-                    
-                )
-                ->whereNotNull('detail_grup_penilaians.id')
-                ->where('gruppenilaians.status', 'Aktif')
-                ->where('users.role', 'Peserta')
-                ->get();
+                'penilaians.nilai_sit_up',
+                'penilaians.jumlah_sit_up',
+
+                'penilaians.nilai_shuttle_run',
+                'penilaians.jumlah_shuttle_run'
+
+            )
+            ->whereNotNull('detail_grup_penilaians.id')
+            ->where('gruppenilaians.status', 'Aktif')
+            ->where('users.role', 'Peserta')
+            ->get();
 
         return response()->json([
             'data'  => $data
         ]);
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
 
         $validator = Validator::make($request->all(), [
 
@@ -58,127 +60,122 @@ class PenilaianController extends Controller
 
         ]);
 
-        if($validator->fails()){
+        if ($validator->fails()) {
             $data = [
                 'responCode'    => 0,
                 'respon'        => $validator->errors()
             ];
-        }else{
+        } else {
 
             //YANG DITES
             $peserta = DB::table('users')
-                        ->join('detail_grup_penilaians', 'detail_grup_penilaians.user_id', '=', 'users.id')
-                        ->where('detail_grup_penilaians.id', $request->detail_grup_penilaian_id)
-                        ->first();
-    
+                ->join('detail_grup_penilaians', 'detail_grup_penilaians.user_id', '=', 'users.id')
+                ->where('detail_grup_penilaians.id', $request->detail_grup_penilaian_id)
+                ->first();
+
             //NILAI REF
             $nilai = DB::table('aturan_nilai_samaptas')
-                        ->where('untuk', $peserta->jk == 'Laki-laki' ? 'Taruna' : 'Taruni')
-                        ->get();
-    
-            if($peserta->jk == 'Laki-laki'){
-    
-                $nilai_lari         = $request->jarak_lari >= 3507.00 ? 
-                                        100 : ($request->jarak_lari <= 1607.00 ? 
-                                        0 : $nilai->where('jenis_samapta', 'Lari')->where('jumlah', $request->jarak_lari)->first());
-    
+                ->where('untuk', $peserta->jk == 'Laki-laki' ? 'Taruna' : 'Taruni')
+                ->get();
+
+            if ($peserta->jk == 'Laki-laki') {
+
+                $nilai_lari         = $request->jarak_lari >= 3507.00 ?
+                    100 : ($request->jarak_lari <= 1607.00 ?
+                        0 : $nilai->where('jenis_samapta', 'Lari')->where('jumlah', $request->jarak_lari)->first());
+
                 $nilai_pushup       = $request->jumlah_push_up >= 43.00 ?
-                                        100 : ( $request->jumlah_push_up <= 16.00 ? 
-                                        0 : $nilai->where('jenis_samapta', 'Push-up')->where('jumlah', $request->jumlah_push_up)->first());
-    
-                $nilai_situp        = $request->jumlah_sit_up >= 41.00 ? 
-                                        100 : ( $request->jumlah_sit_up <= 14.00 ? 
-                                        0 : $nilai->where('jenis_samapta', 'Sit-up')->where('jumlah', $request->jumlah_sit_up)->first());
-    
+                    100 : ($request->jumlah_push_up <= 16.00 ?
+                        0 : $nilai->where('jenis_samapta', 'Push-up')->where('jumlah', $request->jumlah_push_up)->first());
+
+                $nilai_situp        = $request->jumlah_sit_up >= 41.00 ?
+                    100 : ($request->jumlah_sit_up <= 14.00 ?
+                        0 : $nilai->where('jenis_samapta', 'Sit-up')->where('jumlah', $request->jumlah_sit_up)->first());
+
                 $nilai_shuttlerun   = $request->jumlah_shuttle_run >= 15.90 ?
-                                        100 : ( $request->jumlah_shuttle_run <= 25.80 ?
-                                        0 : $nilai->where('jenis_samapta', 'Shuttle Run')->where('jumlah', $request->jumlah_shuttle_run)->first());
-    
-            }else{
-    
-                $nilai_lari         = $request->jarak_lari >= 2630.00 ? 
-                                        100 : ($request->jarak_lari <= 1419.00 ? 
-                                        0 : $nilai->where('jenis_samapta', 'Lari')->where('jumlah', $request->jarak_lari)->first());
-    
+                    100 : ($request->jumlah_shuttle_run <= 25.80 ?
+                        0 : $nilai->where('jenis_samapta', 'Shuttle Run')->where('jumlah', $request->jumlah_shuttle_run)->first());
+            } else {
+
+                $nilai_lari         = $request->jarak_lari >= 2630.00 ?
+                    100 : ($request->jarak_lari <= 1419.00 ?
+                        0 : $nilai->where('jenis_samapta', 'Lari')->where('jumlah', $request->jarak_lari)->first());
+
                 $nilai_pushup       = $request->jumlah_push_up >= 28.00 ?
-                                        100 : ( $request->jumlah_push_up <= 7.00 ? 
-                                        0 : $nilai->where('jenis_samapta', 'Push-up')->where('jumlah', $request->jumlah_push_up)->first());
-    
-                $nilai_situp        = $request->jumlah_sit_up >= 42.00 ? 
-                                        100 : ( $request->jumlah_sit_up <= 14.00 ? 
-                                        0 : $nilai->where('jenis_samapta', 'Sit-up')->where('jumlah', $request->jumlah_sit_up)->first());
-    
+                    100 : ($request->jumlah_push_up <= 7.00 ?
+                        0 : $nilai->where('jenis_samapta', 'Push-up')->where('jumlah', $request->jumlah_push_up)->first());
+
+                $nilai_situp        = $request->jumlah_sit_up >= 42.00 ?
+                    100 : ($request->jumlah_sit_up <= 14.00 ?
+                        0 : $nilai->where('jenis_samapta', 'Sit-up')->where('jumlah', $request->jumlah_sit_up)->first());
+
                 $nilai_shuttlerun   = $request->jumlah_shuttle_run <= 17.20 ?
-                                        100 : ( $request->jumlah_shuttle_run > 27.10 ?
-                                        0 : $nilai->where('jenis_samapta', 'Shuttle Run')->where('jumlah', $request->jumlah_shuttle_run)->first());
-    
+                    100 : ($request->jumlah_shuttle_run > 27.10 ?
+                        0 : $nilai->where('jenis_samapta', 'Shuttle Run')->where('jumlah', $request->jumlah_shuttle_run)->first());
             }
-            
-            if($request->tipe == 'hitung_lari') {
+
+            if ($request->tipe == 'hitung_lari') {
                 Penilaian::updateOrCreate(
                     [
                         'detail_grup_penilaian_id'  => $request->detail_grup_penilaian_id
                     ],
                     [
-                        'jarak_lari'            => $request->jarak_lari ?? null, 
+                        'jarak_lari'            => $request->jarak_lari ?? null,
                         'nilai_lari'            => $nilai_lari->nilai ?? $nilai_lari,
-        
-                        'panitia_id'            => Auth::user()->id ?? 1,
-                        'detail_grup_penilaian_id'  => $request->detail_grup_penilaian_id
-                    ]
-                );
-            } elseif($request->tipe == 'hitung_push_up') {
-                Penilaian::updateOrCreate(
-                    [
-                        'detail_grup_penilaian_id'  => $request->detail_grup_penilaian_id
-                    ],
-                    [
-                        
-                        'jumlah_push_up'        => $request->jumlah_push_up ?? null, 
-                        'nilai_push_up'         => $nilai_pushup->nilai ?? $nilai_pushup, 
-        
+
                         'panitia_id'            => Auth::user()->id ?? 1,
                         'detail_grup_penilaian_id'  => $request->detail_grup_penilaian_id
                     ]
                 );
 
-            } elseif($request->tipe == 'hitung_sit_up') {
+                // UPDATE STATUS
+                ListPesertaInput::where('detail_group_penilaian_id', $request->detail_grup_penilaian_id)->update([
+                    'status' => '1'
+                ]);
+            } elseif ($request->tipe == 'hitung_push_up') {
                 Penilaian::updateOrCreate(
                     [
                         'detail_grup_penilaian_id'  => $request->detail_grup_penilaian_id
                     ],
                     [
-                        'jumlah_sit_up'         => $request->jumlah_sit_up ?? null, 
+
+                        'jumlah_push_up'        => $request->jumlah_push_up ?? null,
+                        'nilai_push_up'         => $nilai_pushup->nilai ?? $nilai_pushup,
+
+                        'panitia_id'            => Auth::user()->id ?? 1,
+                        'detail_grup_penilaian_id'  => $request->detail_grup_penilaian_id
+                    ]
+                );
+            } elseif ($request->tipe == 'hitung_sit_up') {
+                Penilaian::updateOrCreate(
+                    [
+                        'detail_grup_penilaian_id'  => $request->detail_grup_penilaian_id
+                    ],
+                    [
+                        'jumlah_sit_up'         => $request->jumlah_sit_up ?? null,
                         'nilai_sit_up'          => $nilai_situp->nilai ?? $nilai_situp,
-        
+
                         'panitia_id'            => Auth::user()->id ?? 1,
                         'detail_grup_penilaian_id'  => $request->detail_grup_penilaian_id
                     ]
                 );
-
-            } elseif($request->tipe == 'hitung_shuttle_run') {
+            } elseif ($request->tipe == 'hitung_shuttle_run') {
                 Penilaian::updateOrCreate(
                     [
                         'detail_grup_penilaian_id'  => $request->detail_grup_penilaian_id
                     ],
                     [
-                        
-                        'jumlah_shuttle_run'    => $request->jumlah_shuttle_run ?? null, 
+
+                        'jumlah_shuttle_run'    => $request->jumlah_shuttle_run ?? null,
                         'nilai_shuttle_run'     => $nilai_shuttlerun->nilai ?? $nilai_shuttlerun,
-        
+
                         'panitia_id'            => Auth::user()->id ?? 1,
                         'detail_grup_penilaian_id'  => $request->detail_grup_penilaian_id
                     ]
                 );
-
             }
-            
-            
-            // UPDATE STATUS
-            ListPesertaInput::where('detail_group_penilaian_id', $request->detail_grup_penilaian_id)->update([
-                'status' => '1'
-            ]);
-    
+
+
             $data = [
                 'responCode'    => 1,
                 'success' => true,
