de78234c1e5c9f0d2d5e3acdb4c56c0ceee20b74
wahidalqorni <idunalqorni@gmail.com>
2022-09-09 09:26:30 +0700

    update fungsi logout

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use DB;
 
 class LoginController extends Controller
 {
@@ -17,26 +18,26 @@ class LoginController extends Controller
             'password' => 'required',
         ]);
 
-        $user= User::where('email', $request->email)->first();
-        
+        $user = User::where('email', $request->email)->first();
+
         if (!$user || !Hash::check($request->password, $user->password)) {
             return response([
                 'success'   => false,
                 'message' => 'These credentials do not match our records.'
             ], 404);
         }
-        
+
         $token = $user->createToken('ApiToken')->plainTextToken;
-    
+
         $response = [
             'success'   => true,
             'user'      => $user,
             'token'     => $token
         ];
-        
+
         return response($response, 200);
     }
-    
+
     /**
      * logout
      *
@@ -44,9 +45,23 @@ class LoginController extends Controller
      */
     public function logout()
     {
-        auth()->logout();
-        return response()->json([
-            'success'    => true
-        ], 200);
+        $id_user = request('id_user');
+
+        $gettoken = DB::table('personal_access_tokens')->where('tokenable_id', $id_user)->delete();
+
+        if ($gettoken) {
+            auth()->logout();
+
+            return response()->json([
+                'success'    => true
+            ], 200);
+            
+        } else {
+
+
+            return response()->json([
+                'success'    => false
+            ], 500);
+        }
     }
 }
