b00cedbbd70c448e2c24092f2407bf3dbb9b66e9
nabilchen96 <escendol900@gmail.com>
2022-09-20 10:30:55 +0700

    update

--- resources/views/backend/app.blade.php
+++ resources/views/backend/app.blade.php
@@ -139,16 +139,18 @@
                 <!-- partial:partials/_footer.html -->
                 <footer class="footer">
                     <div class="d-sm-flex justify-content-center justify-content-sm-between">
-                        <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Theme By<a
+                        {{-- <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Theme By<a
                                 href="https://www.bootstrapdash.com/" target="_blank">Bootstrap admin
                                 template</a> from BootstrapDash. All rights reserved.</span>
                         <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made
-                            with <i class="ti-heart text-danger ml-1"></i></span>
+                            with <i class="ti-heart text-danger ml-1"></i></span> --}}
                     </div>
                     <div class="d-sm-flex justify-content-center justify-content-sm-between">
-                        <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Distributed by 
-                            <a href="https://www.themewagon.com/" target="_blank">
-                                Themewagon
+                        <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">
+                            Developed by Unit IT Politeknik Penerbangan Palembang <br>
+                            {{-- Distributed by  --}}
+                            <a href="https://poltekbangplg.ac.id/" target="_blank">
+                                poltekbangplg.ac.id
                             </a>
                         </span>
                     </div>
