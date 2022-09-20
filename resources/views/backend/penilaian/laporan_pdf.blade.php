cfa3271984e10d2a0c964bb7bf1522e804a85242
nabilchen96 <escendol900@gmail.com>
2022-09-20 09:22:30 +0700

    update laporan pdf

--- resources/views/backend/penilaian/laporan_pdf.blade.php
+++ resources/views/backend/penilaian/laporan_pdf.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Bukti Pembukuan Jurnal</title>
+    <title>Laporan Penilaian</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
         integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <style>
